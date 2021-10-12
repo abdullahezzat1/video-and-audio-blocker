c2fc9bd5ceed21043d8e37dfb440c47837373429
Abdullah Ezzat Noureddine <abdullahezzat3@gmail.com>
2021-10-12 22:20:33 +0200

    default modes + opposite lists

--- background/bg_1.js
+++ background/bg_1.js
@@ -1,45 +1,74 @@
-let tempUrls = new Set();
-tempUrls.add('www.dailymotion.com');
-tempUrls.add('www.youtube.com');
-
-
-function shouldBeBlocked(hostname) {
-  console.log(hostname);
-  if (tempUrls.has(hostname)) {
-    return true;
+function shouldBeBlocked(hostname, blockByDefault, list) {
+  if (blockByDefault) {
+    if (list.indexOf(hostname) >= 0) {
+      return false;
+    } else {
+      return true;
+    }
   } else {
-    return false;
+    if (list.indexOf(hostname) >= 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
 
-client.webRequest.onHeadersReceived.addListener(
-  function (details) {
+async function main() {
+  await browser.storage.local.set({
+    video: {
+      blockByDefault: true,
+      whiteList: [
+        'www.beinsports.com',
+        'classroom.udacity.com'
+      ],
+      blackList: [],
+    },
+    audio: {
+      blockByDefault: true,
+      whiteList: [],
+      blackList: []
+    }
+  });
+
+  let storage = await browser.storage.local.get();
+
+  browser.webRequest.onHeadersReceived.addListener(function (details) {
     for (const header of details.responseHeaders) {
       if (header.name.toLowerCase() === 'content-type') {
         let type = header.value;
-        if (
-          type.search(/audio/i) >= 0
-          || type.search(/video/i) >= 0
-        ) {
-          let url = details?.frameAncestors[0]?.url ?? details.originUrl;
+        let url = '';
+        let oppositeList = null;
+        let blockByDefault = true;
+        if (type.search(/video/i) >= 0) {
+          url = details?.frameAncestors[0]?.url ?? details.originUrl;
           url = new URL(url);
-          if (shouldBeBlocked(url.hostname)) {
-            return {
-              cancel: true,
-            };
-          }
+          blockByDefault = storage.video.blockByDefault;
+          oppositeList = blockByDefault === true ? storage.video.whiteList : storage.video.blackList;
+        } else if (type.search(/audio/i) >= 0) {
+          url = details?.frameAncestors[0]?.url ?? details.originUrl;
+          url = new URL(url);
+          blockByDefault = storage.audio.blockByDefault;
+          oppositeList = blockByDefault === true ? storage.audio.whiteList : storage.audio.blackList;
         } else {
           return;
+        }
+        if (shouldBeBlocked(url.hostname, blockByDefault, oppositeList)) {
+          return {
+            cancel: true,
+          };
         }
       }
     }
   },
-  {
-    urls: ["<all_urls>"]
-  },
-  ["blocking", "responseHeaders"]
-);
+    {
+      urls: ["<all_urls>"]
+    },
+    ["blocking", "responseHeaders"]
+  );
+
+}
 
 
-
+main()
